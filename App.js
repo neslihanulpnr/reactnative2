c24b056b6538b4ae89a4f84b4d50191a4de754c3
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2023-12-08 14:45:47 +0300

    new

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { Nes } from './Nes'
+import { NavigationContainer } from '@react-navigation/native';
 
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Nes/>
@@ -12,6 +14,7 @@ export default function App() {
       <Button title="Modal"/>
 
     </View>
+    </NavigationContainer>
   );
 }
 
